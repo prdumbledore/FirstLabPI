7e20b25346f2a49fe55de69f11213e177b9d3017
Erik Sargsyan <sargsyan.ee@gmail.com>
2022-11-29 01:00:00 +0300

    #3 e4
    add Caesar packing mode
    add PackCaesar.java
    add packing function

--- src/main/kotlin/com/eriksargsyan/plugins/Routing.kt
+++ src/main/kotlin/com/eriksargsyan/plugins/Routing.kt
@@ -1,23 +1,36 @@
 package com.eriksargsyan.plugins
 
+import PackCaesar
 import PackRLE
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 const val OPERATION = "operation"
-const val ENCRYPT = "text"
+const val STEP = "step"
+const val TEXT = "text"
 val packRLE = PackRLE()
+val packCaesar = PackCaesar()
 
 fun Application.configureRouting() {
 
     routing {
-        get("/encrypt") {
+        get("/rle") {
             val operationType = call.parameters[OPERATION]
-            val encryptText = call.parameters[ENCRYPT]
+            val encryptText = call.parameters[TEXT]
             when (operationType) {
                 "p", "u" -> {
                     call.respondText(packRLE.newEncrypt(true, encryptText))
+                }
+            }
+        }
+        get("/caesar") {
+            val operationType = call.parameters[OPERATION]
+            val shiftKey = call.parameters[STEP]
+            val encryptText = call.parameters[TEXT]
+            when (operationType) {
+                "p", "u" -> {
+                    call.respondText(packCaesar.newEncrypt(true, encryptText, shiftKey!!.toInt()))
                 }
             }
         }
